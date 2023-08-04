b117aea93bcc0a306c8f4ba0908ed57d36b3f938
Quintus Marais <quintus.marais@kwilibri.com>
2023-08-04 14:54:28 +0200

    Add viewport meta tag

--- src/WebServer/MasterPage.fs
+++ src/WebServer/MasterPage.fs
@@ -7,6 +7,9 @@ module MasterPage =
         html [ _lang "en"] [
             head [] [
                 meta [ _charset "utf-8" ]
+                meta [ 
+                    _name "viewport" 
+                    _content "width=device-width, initial-scale=1.0"]
                 title [] [encodedText pageTitle]
             ]
             body [] [
