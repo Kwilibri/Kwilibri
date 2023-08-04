74ca45d7775f2e5fc61b333ffddba738f608c4ee
Quintus Marais <quintus.marais@kwilibri.com>
2023-08-04 17:04:06 +0200

    Add content as per previous website

--- src/WebServer/WebApp.fs
+++ src/WebServer/WebApp.fs
@@ -10,7 +10,31 @@ module WebApp =
 
     let indexGet () =
         fun (next: HttpFunc) (ctx: HttpContext) ->
-            let pageContent = [ p [] [ encodedText "Hello World" ] ]
+            let pageContent =
+                [ div [] []
+                  section
+                      []
+                      [ p
+                            []
+                            [ encodedText
+                                  "Kwilibri is a software and services company based in Johannesburg, South Africa." ]
+                        p
+                            []
+                            [ encodedText
+                                  "We help organizations and busy people get things done while being compliant with relevant legislation and regulations." ]
+                        p
+                            []
+                            [ encodedText
+                                  "For more information you can contact us on info@kwilibri.com or by phone at (+ 27)11 083 6706." ]
+                        p
+                            []
+                            [ encodedText "You can also log a service request on our "
+                              a [ _href "https://kwilibri.freshdesk.com/" ] [ rawText "support portal." ] ] ]
+                  div [] []
+                  div [] []
+                  div [] []
+                  div [] [] ]
+
             let view = pageContent |> masterPage "Kwilibri"
             (htmlView view) next ctx
 
